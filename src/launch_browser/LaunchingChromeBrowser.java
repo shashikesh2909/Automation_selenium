6ee03ca39f6f079c9768ad492e045f0e1f51bcf9
Shashi <shashikesh2909@gmail.com>
2024-05-20 16:24:53 +0530

    commit

---
+++ src/launch_browser/LaunchingChromeBrowser.java
@@ -0,0 +1,26 @@
+package launch_browser;
+
+import java.util.Scanner;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class LaunchingChromeBrowser 
+{
+	static Scanner sc = new Scanner(System.in) ;
+	public static void main (String [] args)
+	{
+		System.out.println("PLease enter which browser to open chrome/edge");
+		String s = sc.next();
+		
+		if(s.equalsIgnoreCase("chrome"))     
+			{
+				ChromeDriver cdriver = new ChromeDriver() ;
+			}
+		else if (s.equalsIgnoreCase("edge"))
+		{
+			EdgeDriver edriver = new EdgeDriver() ;
+			edriver.close();
+		}
+		
+	}
+}
